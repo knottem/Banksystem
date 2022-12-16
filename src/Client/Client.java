eea296771d739f62b9471b6e1c3807228b9d7a33
knottem <59744487+knottem@users.noreply.github.com>
2022-12-16 09:20:15 +0100

    Client and utility added

---
+++ src/Client/Client.java
@@ -0,0 +1,51 @@
+package Client;
+
+import utility.Utility;
+
+import java.time.LocalDate;
+
+public class Client {
+
+    LocalDate today = LocalDate.now();
+    Utility utility = new Utility();
+    FAQ faq = new FAQ();
+    boolean repeat = false;
+
+    void Program() {
+
+        System.out.println("\nVälkommen till Bank Systemet");
+        System.out.println("Dagens Datum: " + today + "\n");
+
+        do {
+            boolean startLoop = true;
+            while (startLoop) {
+                int answer = utility.inputInt("""
+                        Vad vill du göra?
+                        1. Logga in
+                        2. Skapa Nytt Konto
+                        3. FAQ
+                        4. Avsluta Programmet""");
+                switch (answer) {
+                    case (1) -> {
+                    }
+                    case (2) -> {
+                    }
+                    case (3) -> {
+                        faq.print();
+                        utility.sleep(1000);
+                        startLoop = false;
+                    }
+                    case (4) -> System.exit(0);
+
+                    default -> System.out.println("Felaktigt nummer");
+                }
+            }
+            repeat = utility.repeatProgram("\nKör igen?");
+        }while (!repeat);
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.Program();
+    }
+}
