df5c73094f471ed96ab9eb7fa22530e0f385308b
Frida Lindberg <fridalindberg03@gmail.com>
2022-12-16 10:46:22 +0100

    read admins from file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import Client.Window;
+import Users.Customer;
+import Users.User;
+
+import java.util.concurrent.CyclicBarrier;
 
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
         window.setupLogin();
         window.setupLoginPage();
+        new Database();
 
     }
 }
