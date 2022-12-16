137903104fb317f75e7f7309042c8665fa792f5e
SamuelPeters0n <sampet02@gmail.com>
2022-12-16 09:49:31 +0100

    Added writeToFile in History

--- src/History.java
+++ src/History.java
@@ -1,9 +1,20 @@
-import java.io.File;
+import Users.User;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class History {
-    File file = new File("src/history.txt");
+    File file = new File("resources/history.txt");
 
-    public void writeToFile(){
+    public void writeToFile(String text, User user) {
+        try(BufferedWriter print = new BufferedWriter(new FileWriter(file, true))){
 
+            String textForHistoryTextFile = "User: "+ user.getName() + ". Activity: " + text + ". Time: " + LocalTime.now();
+            print.write(textForHistoryTextFile);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import Client.Window;
+import Users.Admin;
+import Users.User;
 
 public class Main {
     public static void main(String[] args) {
+
         Window window = new Window();
         window.setupLogin();
         window.setupLoginPage();
